6c789b984f6112c44756aaa3088efb74e972a8f3
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-05-04 23:24:04 +0200

    minorUpd

--- Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/MainActivity.java
+++ Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.front_grocery_android;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imageButtonHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButtonHelp = findViewById(R.id.imageButtonHelp);
+
+        imageButtonHelp.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("//TODO");
+            builder.setPositiveButton("Close", (dialog, id) -> {
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
     }
 }
