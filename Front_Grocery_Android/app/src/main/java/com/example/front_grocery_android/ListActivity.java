476a590df48083a389b11d4c0aa847b6dea4b2c2
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-05-05 17:32:00 +0200

    smallUpd

--- Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/ListActivity.java
+++ Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/ListActivity.java
@@ -3,12 +3,22 @@ package com.example.front_grocery_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
+
+    private TextView textViewListId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        textViewListId = findViewById(R.id.textView_list_id);
+
+        //extract params
+        Bundle bundle = getIntent().getExtras();
+        String listId = bundle.getString("listId");
+        textViewListId.setText(listId);
     }
 }
--- Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/MainActivity.java
+++ Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.front_grocery_android;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton imageButtonHelp;
     private Button buttonGoToList;
     private Button buttonGenerate;
+    private EditText editTextListId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         imageButtonHelp = findViewById(R.id.imageButtonHelp);
         buttonGoToList = findViewById(R.id.button_go_to_list);
         buttonGenerate = findViewById(R.id.button_generate);
+        editTextListId = findViewById(R.id.editText_list_id);
 
         //help button
         imageButtonHelp.setOnClickListener(v -> {
@@ -33,7 +38,15 @@ public class MainActivity extends AppCompatActivity {
 
         //go to list button
         buttonGoToList.setOnClickListener(v -> {
-            System.out.println("");
+            if (TextUtils.isEmpty(editTextListId.getText())) {
+                editTextListId.setError("This field is required");
+            } else if ((Integer.parseInt(editTextListId.getText().toString()) > 9999) || (Integer.parseInt(editTextListId.getText().toString()) < 1000)) {
+                editTextListId.setError("The value should be between 1000 and 9999");
+            } else {
+                Intent toList = new Intent(this, ListActivity.class);
+                toList.putExtra("listId", editTextListId.getText().toString());
+                startActivity(toList);
+            }
         });
 
         //generate button
