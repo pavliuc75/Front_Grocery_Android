7b6e188fe3620f0777e8a9209021eab52275f141
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-05-11 22:44:46 +0200

    models added

---
+++ Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/models/Item.java
@@ -0,0 +1,11 @@
+package com.example.front_grocery_android.models;
+
+public class Item {
+    public int id;
+    public String name;
+    public String details;
+    public boolean isCompleted;
+    public int quantity;
+    public double weight;
+    public String unit;
+}
---
+++ Front_Grocery_Android/app/src/main/java/com/example/front_grocery_android/models/List.java
@@ -0,0 +1,9 @@
+package com.example.front_grocery_android.models;
+
+import java.util.ArrayList;
+
+public class List {
+    public int id;
+    public String description;
+    public ArrayList<Item> items;
+}
